f26b7bc346a9a0546db4c27ab90cbfe7869a23b9
Silas Ribeiro <santorsilas@gmail.com>
2024-11-03 15:54:19 -0300

    feat: create .nv_history

--- nv.sh
+++ nv.sh
@@ -5,9 +5,9 @@ SCRIPT_DIR=$(cd "$(dirname "$(readlink -f "$0")")" && pwd)
 if [ -z "$1" ]; then
   PWD=$(pwd)
 elif [ -d "$1" ]; then
-  PWD="$1"
+  PWD=$(cd "$1" && pwd)
 elif [ -f "$1" ]; then
-  FILE_PATH="$1"
+  FILE_PATH=$(readlink -f "$1")
   PWD=$(dirname "$FILE_PATH")
 else
   echo "Error: Invalid path provided."
@@ -26,10 +26,24 @@ $([ -n "$FILE_PATH" ] && echo "    args \"$FILE_PATH\"")
 }
 EOL
 
+CONFIG_DIR="$SCRIPT_DIR/configs"
+
 alacritty \
   --working-directory "$PWD" \
   --class Alacritty \
-  --config-file "$SCRIPT_DIR/alacritty/alacritty.toml" \
-  -o "env.ZELLIJ_CONFIG_FILE=\"$SCRIPT_DIR/zellij/config.kdl\"" \
+  --config-file "$CONFIG_DIR/alacritty.toml" \
+  -o "env.ZELLIJ_CONFIG_FILE=\"$CONFIG_DIR/zellij.kdl\"" \
   -e zellij \
   --layout="$LAYOUT_FILE" &
+
+# Create .nv_history file in $HOME if it doesn't exist
+NV_HISTORY_FILE="$HOME/.nv_history"
+[ ! -f "$NV_HISTORY_FILE" ] && touch "$NV_HISTORY_FILE"
+
+# Store PWD, FILE_PATH, and timestamp in .nv_history
+TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
+if [ -n "$FILE_PATH" ]; then
+  echo "$TIMESTAMP | PWD=$PWD | FILE_PATH=$FILE_PATH" >>"$NV_HISTORY_FILE"
+else
+  echo "$TIMESTAMP | PWD=$PWD" >>"$NV_HISTORY_FILE"
+fi
